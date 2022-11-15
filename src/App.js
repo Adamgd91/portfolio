8705587318c8872173e4e185649dabe3def3dc5b
Adam Davidson <agdavidson91@gmail.com>
2022-11-14 19:21:11 -0500

    inital project setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import AboutMe from "./components/AboutMe/AboutMe";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+import NavBar from "./components/NavBar/NavBar";
+import Projects from "./components/Projects/Projects";
+import "./main.scss";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <NavBar />
+      <Hero />
+      <AboutMe />
+      <Projects />
+      <Footer />
     </div>
   );
 }
